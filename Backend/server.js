70d2347d92040f75910245c52f39076ce70ed597
Nitin <nitinbrijwal999@gmail.com>
2023-02-06 09:22:42 +0530

    commit 5

--- Backend/server.js
+++ Backend/server.js
@@ -11,9 +11,8 @@ const app  = express()
 app.use(express.json());
 app.use(express.urlencoded({extended:false}));
 app.use(cors({
-    origin:"*",
-})
-);
+    origin:"*"
+}));
 app.use(taskRoutes);
 
 //routes
